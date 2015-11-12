8f867c0d1890e597939c4a79ed1f7db209072556
DenDoronin <den.doronind@gmail.com>
2015-11-12 13:45:40 +0200

    Initial Commit

---
+++ MyFriends/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFriends
+//
+//  Created by Doronin Denis on 11/12/15.
+//  Copyright © 2015 Doronin Denis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
